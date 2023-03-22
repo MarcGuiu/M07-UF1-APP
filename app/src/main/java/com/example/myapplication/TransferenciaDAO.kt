bc5ac8109021f42f091db9e0b4479f54eea44b79
Pedro Marrodan <pedromarrodanhernandez@Ordenador-portatil-de-Pedro.local>
2023-03-22 23:15:34 +0100

    Insert new transfer in db

--- app/src/main/java/com/example/myapplication/TransferenciaDAO.kt
+++ app/src/main/java/com/example/myapplication/TransferenciaDAO.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.Companion.REPLACE
 import androidx.room.Query
@@ -9,6 +11,9 @@ import androidx.room.Query
 interface TransferenciaDAO {
     @Insert(onConflict = REPLACE)
     fun insert(transferencia: Transferencia)
+
+    @Delete
+    fun delete(vararg transferencia: Transferencia)
 
     @Query("SELECT * FROM transferencia")
     fun loadAllTransfers(): List<Transferencia>
