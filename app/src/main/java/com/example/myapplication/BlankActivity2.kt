4de053d1c61e29fafc9f744e94cfe08986d8c311
Pedro Marrodan <pedromarrodanhernandez@Ordenador-portatil-de-Pedro.local>
2023-03-23 20:38:11 +0100

    Unir codis

--- app/src/main/java/com/example/myapplication/BlankActivity2.kt
+++ app/src/main/java/com/example/myapplication/BlankActivity2.kt
@@ -1,12 +1,23 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class BlankActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blank2)
+        val bundle = intent.extras
+        val transferencia: Transferencia? = bundle?.getSerializable("transferencia") as Transferencia?
+
+        val bundle2 = Bundle()
+        bundle2.putSerializable("transferencia", transferencia)
+        val fragment = ResumFragment()
+        fragment.arguments = bundle2
+        supportFragmentManager.beginTransaction()
+//            .add(R.id.resum, fragment)
+            .add(R.id.resum, ResumFragment.newInstance(transferencia!!))
+
+            .commit()
     }
 }
